ef7261b91d403f45b2d6d21336643a65214f46ab
sindorei <wupan1030@foxmail.com>
2015-10-20 23:36:12 +0800

    asp.net mvc

---
+++ ASP.NET/asp.net-MVC.md
@@ -0,0 +1,23 @@
+# ViewData 和 ViewBag
+数据从控制器传送到视图 通过 ViewData（ViewDataDictionary字典类）
+
+`ViewData["CurrentTime"] = DateTime.Now;`
+
+ViewBag 是 ViewData的动态封装器
+
+ViewBag.CurrentTime 等同于 ViewData["CurrentTime"]
+
+差异：
+- 当要访问的关键字是一个有效的C#标识符时，ViewBag才起作用
+- 动态值不能作为参数传递给扩展方法（C#编译器为了选择正确的扩展方法，在编译时必须知道每个参数的真正类型） 使用ViewData或把ViewBag转成具体的类型 如 (string)ViewBag.Name
+
+# 强类型视图
+
+告知视图哪种类型的模型正在使用 @model声明  
+
+需输入模型类型的完全限定类型名（名称空间和类型名称）
+
+如果不想输入模型的完全限定类型名，可使用@using关键字声明
+对于视图中经常用的命名空间，可在Views目录下的web.config文件中声明
+
+# 视图模型
--- NodeJs/demo/1.js
+++ NodeJs/demo/1.js
@@ -1,7 +1,18 @@
 var http = require('http');
-var url = require('url');
-http.createServer(function(request,response){
-    response.writeHead(200,{"Content-Type":"text/html;charset=utf-8"});
-    response.write(JSON.stringify(url.parse(request.url,true).query));
-    response.end()
-}).listen(8080);
+//var url = require('url');
+//http.createServer(function(request,response){
+//    response.writeHead(200,{"Content-Type":"text/plain;charset=utf-8"});
+//    var oQuery = url.parse(request.url,true).query;
+//    var name = oQuery.name ? oQuery.name : '';
+//    response.write(JSON.stringify({name:name,age:22}));
+//    response.end()
+//}).listen(8080);
+http.get('http://www.baidu.com',function(res) {
+    var html = '';
+    res.on('data',function(data) {
+        html += data;
+    })
+    res.on('end',function() {
+        console.log(html);
+    })
+})
