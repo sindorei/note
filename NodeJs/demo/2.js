96d1209213691e5b0d28ec52748dd3d0114655fa
sindorei <wupan1030@foxmail.com>
2015-11-29 20:51:57 +0800

    sql

--- NodeJs/demo/2.js
+++ NodeJs/demo/2.js
@@ -1,2 +1,21 @@
-var tp = '唐培';
-module.exports.tp = tp;
+//var name = '';
+//var age = '';
+//process.stdout.write("请输入姓名：");
+//process.stdin.resume();
+//process.stdin.on('data',function(content) {
+//    if(!name)
+//    {
+//        name = content;
+//        process.stdout.write("请输入年龄：");
+//    }
+//    else
+//    {
+//        age = content;
+//        process.stdout.write("你好"+name);
+//    }
+//})
+
+var fs = require('fs');
+fs.readFile('1.txt',function(err , data){
+    console.log(data.toString());
+})
