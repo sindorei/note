0bc7e743ef003a6a687027e9de0df5b0cb978975
sindorei <wupan1030@foxmail.com>
2016-08-22 20:35:01 +0800

    webpack

---
+++ 前端工具/webpack/src/voucher/views/demo/Demo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img class="logo" src="../../assets/images/logo.png">
+    <hello></hello>
+    <p>
+      Welcome to voucher Demo 哈哈哈
+    </p>
+    <toast :show="false"></toast>
+  </div>
+</template>
+
+<script>
+import Hello from '../../components/Hello'
+import Toast from '../../components/Toast'
+
+export default {
+  components: {
+    Hello,
+    Toast
+  }
+}
+</script>
+
+<style lang="sass">
+@import "../../assets/style/demo";
+</style>
---
+++ 前端工具/webpack/src/weixin/views/demo/Demo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <img class="logo" src="../../assets/images/logo.png">
+    <hello></hello>
+    <p>
+      Welcome to Demo
+    </p>
+    <toast :show="true"></toast>
+  </div>
+</template>
+
+<script>
+import Hello from '../../components/Hello'
+import Toast from '../../components/Toast'
+
+export default {
+  components: {
+    Hello,
+    Toast
+  }
+}
+</script>
+
+<style lang="sass">
+@import "../../assets/style/demo";
+</style>
