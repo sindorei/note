de6061b7efd1f01c819af30cb9279c9e1350f230
sindorei <wupan1030@foxmail.com>
2022-01-13 11:24:17 +0800

    emmm

---
+++ rust/doc.md
@@ -0,0 +1,4 @@
+# 相关工具
+- rustup （Rust版的nvm）
+    * 负责管理Rust的安装以及额外的编译目标（如WebAssembly），除此之外还包含一些核心的工具，如cargo（Rust版的npm）、clippy（Rust版的eslint）、rustfmt（Rust版的prettier）
+    * [安装](https://www.rust-lang.org/tools/install)
---
+++ swift/demo/Landmarks/Landmarks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Landmarks
+//
+//  Created by sindorei on 2021/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
