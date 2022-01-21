f35c4f139b0039ad9cec8859e9eb2a29c12f722f
sindorei <wupan1030@foxmail.com>
2022-01-21 16:05:20 +0800

    vuepress

---
+++ docs/.vuepress/config.ts
@@ -0,0 +1,12 @@
+import { defineUserConfig } from 'vuepress'
+import type { DefaultThemeOptions } from 'vuepress'
+
+export default defineUserConfig<DefaultThemeOptions>({
+  lang: 'zh-CN',
+  title: '笔记',
+  description: '平时学习及练习是时的记录',
+  theme: '@vuepress/theme-default',
+  themeConfig: {
+    logo: 'https://vuejs.org/images/logo.png',
+  },
+})
---
+++ docs/README.md
@@ -0,0 +1,2 @@
+# 前端
+- [JavaScript设计模式](./frontend/javascript/design_pattern.md)
