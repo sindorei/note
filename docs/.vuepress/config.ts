03ddecb5b4d7a2ac499c05c156cb63336b148e8d
sindorei <wupan1030@foxmail.com>
2022-01-21 16:31:13 +0800

    vuepress config

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -5,6 +5,7 @@ export default defineUserConfig<DefaultThemeOptions>({
   lang: 'zh-CN',
   title: '笔记',
   description: '平时学习及练习是时的记录',
+  base: '/note/',
   theme: '@vuepress/theme-default',
   themeConfig: {
     logo: 'https://vuejs.org/images/logo.png',
