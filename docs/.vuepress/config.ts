fc2e6ad0f09b61aa32e557258c5479f530d6b8b8
sindorei <wupan1030@foxmail.com>
2022-11-25 15:40:47 +0800

    desc

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { sidebar } from './sidebar'
 export default defineUserConfig({
   lang: 'zh-CN',
   title: '笔记',
-  description: '平时学习及练习是时的记录',
+  description: '学习及练习的记录',
   base: '/note/',
   theme: defaultTheme({
     sidebarDepth: 1,
